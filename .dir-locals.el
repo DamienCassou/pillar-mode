de8d504faf5ec22d8d25086d4ccfd9824492a297
Damien Cassou <damien.cassou@gmail.com>
2014-01-30 18:13:22 +0100

    Configure some variables in emacs-lisp-mode

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((emacs-lisp-mode
+  (sentence-end-double-space . t)
+  (eval flycheck-mode)
+  (eval checkdoc-minor-mode)))
+
+;; Local Variables:
+;; mode: fundamental
+;; End:
